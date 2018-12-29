477c80fcc4f1e3a9bdab185ee4d910b84243f1a9
madmaul <amaul600@gmail.com>
2018-12-29 12:11:45 +0700

    NewGit
    Java Buku dan AddBuku, tapi aku beum periksa bisa masuk atau enggak ke db nya soalnya aku lupa struktur db nya

---
+++ app/src/main/java/id/ac/unri/ursmartlibrary/Buku.java
@@ -0,0 +1,30 @@
+package id.ac.unri.ursmartlibrary;
+
+public class Buku {
+
+    String judul;
+    String pengarang;
+    String tahun;
+
+    public Buku () {
+
+    }
+
+    public Buku (String judul, String pengarang, String tahun) {
+        this.judul = judul;
+        this.pengarang = pengarang;
+        this.tahun = tahun;
+    }
+
+    public String getJudulBuku() {
+        return judul;
+    }
+
+    public String getPengarangBuku() {
+        return pengarang;
+    }
+
+    public String getTahunTerbit() {
+        return tahun;
+    }
+}
---
+++ app/src/main/java/id/ac/unri/ursmartlibrary/addBookActivity.java
@@ -0,0 +1,55 @@
+package id.ac.unri.ursmartlibrary;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.text.TextUtils;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+public class addBookActivity extends AppCompatActivity {
+
+    EditText etJudulBuku;
+    EditText etPengarangBuku;
+    EditText etTahunBuku;
+    Button buttonAddBook;
+
+    DatabaseReference databaseBuku;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_add_book);
+
+        etJudulBuku = (EditText) findViewById(R.id.etJudulBuku);
+        etPengarangBuku = (EditText) findViewById(R.id.etPengarangBuku);
+        etTahunBuku = (EditText) findViewById(R.id.etTahunBuku);
+
+        databaseBuku = FirebaseDatabase.getInstance().getReference("Buku");
+
+        buttonAddBook.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                addBook();
+            }
+        });
+    }
+
+    private void addBook () {
+        String judul = etJudulBuku.getText().toString().trim();
+        String pengarang = etPengarangBuku.getText().toString().trim();
+        String tahun = etTahunBuku.getText().toString();
+
+        if(!TextUtils.isEmpty(judul) || !TextUtils.isEmpty(pengarang) || !TextUtils.isEmpty(tahun)) {
+            String id = databaseBuku.push().getKey();
+            Buku buku = new Buku(id, judul, pengarang, tahun);
+
+            databaseBuku.child(id).setValue(buku);
+            Toast.makeText(this, "Buku telah ditambahkan", Toast.LENGTH_LONG).show();
+        } else {
+            Toast.makeText(this, "Judul, pengarang dan tahun buku tidak boleh kosong!", Toast.LENGTH_LONG).show();
+        }
+    }
+
+}
