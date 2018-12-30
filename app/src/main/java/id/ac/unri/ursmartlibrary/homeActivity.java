e06b7b2bb224ec31efc40099ba16f9a8b5cbef6e
madmaul <amaul600@gmail.com>
2018-12-30 14:36:28 +0700

    Test

--- app/src/main/java/id/ac/unri/ursmartlibrary/homeActivity.java
+++ app/src/main/java/id/ac/unri/ursmartlibrary/homeActivity.java
@@ -1,7 +1,10 @@
 package id.ac.unri.ursmartlibrary;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class homeActivity extends AppCompatActivity {
 
@@ -9,5 +12,31 @@ public class homeActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home);
+    }
+
+    public void onClick(View v) {
+        Intent intent = null;
+        switch (v.getId()) {
+            case R.id.btnProfilHome:
+                intent = new Intent(this, profile.class);
+                startActivity(intent);
+                break;
+            case R.id.btnDaftarBukuHome:
+                intent = new Intent(this, daftarBukuActivity.class);
+                startActivity(intent);
+                break;
+            case R.id.btnTambahBukuHome:
+                intent = new Intent(this, addBookActivity.class);
+                startActivity(intent);
+                break;
+            case R.id.btnPinjamBukuHome:
+                intent = new Intent(this, pinjamBukuActivity.class);
+                startActivity(intent);
+                break;
+            case R.id.btnAboutHome:
+                intent = new Intent(this, about.class);
+                startActivity(intent);
+                break;
+        }
     }
 }
